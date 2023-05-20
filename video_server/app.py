feb93dc423b4209fd4e6412b3e54b61214d049bf
jxxxxharu <jxxxxharu@gmail.com>
2023-05-20 14:19:03 +0900

    [Feat] DASH, HLS 동영상  스트리밍 서버

--- video_server/app.py
+++ video_server/app.py
@@ -7,11 +7,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route('/video/<path:filename>')
 def video(filename):
     return send_from_directory('videos', filename)
 
+@app.route('/hls/<path:filename>')
+def hls(filename):
+    return send_from_directory('hls', filename)
+
+@app.route('/hls')
+def hls_index():
+    return render_template('hls.html')
+
+@app.route('/dash/<path:filename>')
+def dash(filename):
+    return send_from_directory('dash', filename)
+
+@app.route('/dash')
+def dash_index():
+    return render_template('dash.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
